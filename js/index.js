5078037424b5fea9c1fb21bde34f200e17699832
Hari Krishna Vetharenian <nus.harikv@gmail.com>
2015-07-03 11:20:59 +0400

    Remove console logs and referenced files hosted on github

--- js/index.js
+++ js/index.js
@@ -34,21 +34,19 @@ function relMouseCoords(event){
 }
 
 window.onload = function(){
-	console.log('Loaded');
+	console.log('Customator Loaded');
 	var	lazyloader = document.createElement("script");
 	lazyloader.type = "text/javascript";
 	lazyloader.async = !0;
-	lazyloader.src="https://f2be67ea0ae8d203e5c981405fe4519423aee692.googledrive.com/host/0B6BJml1-TTkLfkxhMjRDRGc1Q0RfWXdyd3h3QThPWlpoZjh0NXBYek1LVUVDY01URExjcGs/lazyload.js";
+	lazyloader.src="http://customator.io/ingestion-js/js/lazyload.js";
 	var n=document.getElementsByTagName("script")[0];
 	n.parentNode.insertBefore(lazyloader,n);
 	lazyloader.onload = function(){
-		LazyLoad.js(['https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js', 'js/common-web.js'], function(){
+		LazyLoad.js(['https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js', 'http://customator.io/ingestion-js/js/common-web.js'], function(){
 			CommonWeb.Callback = function(collection, properties, callback){
-				console.log(properties);
 				var s = serialize(properties);
 				httpRequest.open("get", "http://128.199.64.221:9880/customator.dev?json="+encodeURIComponent(JSON.stringify(properties)), true);
 				httpRequest.send();
-				console.log(s)
 			};
 			CommonWeb.addGlobalProperties({
 				page_info: {
@@ -77,9 +75,7 @@ window.onload = function(){
         };
       });
 			CommonWeb.trackClicksPassive($("input"), function(event){
-				console.log(event);
 				mouse_coords = relMouseCoords(event);
-				console.log(mouse_coords);
         return {
           mouse_coords,
           time: {
